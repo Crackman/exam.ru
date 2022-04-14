b83de987ebb9439ec83f4db03586b0a0bb4fe2a3
Baldis <bitochek67@mail.ru>
2022-04-14 15:26:02 +0300

    Update content

--- resources/views/russian_lang/spelling/spelling.blade.php
+++ resources/views/russian_lang/spelling/spelling.blade.php
@@ -4,14 +4,14 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Орфография (пpaвилa нaпиcaния cлoв) pyccкoгo языкa</h1>
+                <h1>Орфография (правила написания слов) русского языка</h1>
 
                 <ul class="nav flex-column">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Об орфографии</a>
                         <ul class="nav flex-column mx-5">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Чтo тaкoe opфoгpaфия?</a>
+                                <a class="nav-link" href="#">Чтo такое орфография?</a>
                             </li>
                             <li class="nav-item">
                                 <a class="nav-link" href="#">Зачем нужна орфография русского языка?</a>
